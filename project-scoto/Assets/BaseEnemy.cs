bc5a193ede62d1d99b1f90e65bd3fa2fc13d86ac
Hayden Carroll <haydencarroll77@gmail.com>
2021-09-29 15:32:57 -0700

    created base enemy class + enemy prefab

---
+++ project-scoto/Assets/BaseEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 m_position;
+    BaseEnemy(Vector3 position)
+    {
+        // spawn at that position
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
