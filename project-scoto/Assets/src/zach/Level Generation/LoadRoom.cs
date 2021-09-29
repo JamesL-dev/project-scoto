ff5f3adea3b76dc25dbeb7495dd4ce85a255ea19
ZachAttack6089 <70180035+ZachAttack6089@users.noreply.github.com>
2021-09-29 16:53:29 -0700

    Fixed errors

--- project-scoto/Assets/src/zach/Level Generation/LoadRoom.cs
+++ project-scoto/Assets/src/zach/Level Generation/LoadRoom.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadRoom : MonoBehaviour {
-    public BaseEnemy enemy;
+    public GameObject enemy;
 
     private void Start() {
         enemy = Instantiate(enemy);
