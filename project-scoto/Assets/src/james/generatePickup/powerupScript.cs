0ba3366154c71012b5955a145883d28884549bed
JamesL-dev <jmlasso2@nic.edu>
2021-09-29 19:51:16 -0700

    Add power up prefab and spawn

---
+++ project-scoto/Assets/src/james/generatePickup/powerupScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerupScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 m_position;
+    powerup(Vector3 position)
+    {
+        // spawn at that position
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Object destroyed when player collides with
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.name = "Player")
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+}
