0e618e7bbc5b4d6ff78ef60762f9a44ea61353d2
RodneyMcCoy <rbmj2001@outlook.com>
2021-10-03 13:18:58 -0700

    a
    a

--- project-scoto/Assets/src/rodney/Unity/Debugging.cs
+++ project-scoto/Assets/src/rodney/Unity/Debugging.cs
@@ -15,14 +15,20 @@ public class Debugging : MonoBehaviour
         DEBUGGING = weapon_input_actions.Player.DEBUGGING;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if(timer != 0) {timer --;}
+        if(timer > 0) {timer --;}
+
         if(DEBUGGING.ReadValue<float>() != 0 && timer == 0) {
-            // Debug.Log("Debugging: " + curr_state.ToString());
+            /* This line is what allows you to turn the inventory on and off. The first part gives access to the function EnableInventory.
+            The second part, EnableInventory(bool var), turns the inventory ON if var is true (and if the inventory is currently off) and it turns the
+            inventory OFF if var is false (and the inventory is currently on).
+            Feel free to use this line in code if needed but i might be changing it to be more simple, by using a namespace,
+            so be wary of if i change, which i will notify discord if i do */
             GameObject.Find("Inventory").GetComponent<WeaponManager>().EnableInventory(!curr_state);
-            curr_state = !curr_state;
-            timer = 120;
+            
+            curr_state = !curr_state; // Keep track of current state of inventory
+            timer = 60; // Only toggle inventory every 60 frames
         }
     }
 
