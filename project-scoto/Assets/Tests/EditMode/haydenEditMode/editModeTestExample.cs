e84dc071b1e40ad2318521ad353e6f874ea5bdeb
Hayden Carroll <haydencarroll77@gmail.com>
2021-10-06 12:10:53 -0700

    added tests

--- project-scoto/Assets/Tests/EditMode/haydenEditMode/editModeTestExample.cs
+++ project-scoto/Assets/Tests/EditMode/haydenEditMode/editModeTestExample.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
+// using System.Collections;
+// using System.Collections.Generic;
+// using NUnit.Framework;
+// using UnityEngine;
+// using UnityEngine.TestTools;
 
-public class EditModeTestExample
-{
-    // A Test behaves as an ordinary method
-    [Test]
-    public void Test1()
-    {
-        int number1 = 5;
-        int number2 = 2;
-        Assert.AreEqual(number1, number2);
-    }
+// public class EditModeTestExample
+// {
+//     // A Test behaves as an ordinary method
+//     [Test]
+//     public void Test1()
+//     {
+//         int number1 = 5;
+//         int number2 = 2;
+//         Assert.AreEqual(number1, number2);
+//     }
 
-    [Test]
-    public void Test2()
-    {
-        Assert.IsTrue(0 <= 1);
-    }
+//     [Test]
+//     public void Test2()
+//     {
+//         Assert.IsTrue(0 <= 1);
+//     }
 
-}
+// }
