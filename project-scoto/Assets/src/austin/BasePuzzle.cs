a637d4fbe2eff7695b32b9c825acaf6f64005d2f
unknown <austinpkugler@gmail.com>
2021-10-06 10:28:44 -0700

    Initial OdysseyPuzzle implementation

--- project-scoto/Assets/src/austin/BasePuzzle.cs
+++ project-scoto/Assets/src/austin/BasePuzzle.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 
 public class BasePuzzle : MonoBehaviour
 {
-    [SerializeField] protected bool isActive = false;
-    [SerializeField] protected float frequency = 0.5f;
-    [SerializeField] protected int delay = 300;
+    [SerializeField] protected bool m_isActive = false;
+    [SerializeField] protected float m_frequency = 0.5f;
+    [SerializeField] protected int m_delay = 300;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_frequency < 0.0f)
+        {
+            m_frequency = 0.0f;
+        }
+        else if (m_frequency > 1.0f)
+        {
+            m_frequency = 1.0f;
+        }
+    }
+
+    public bool IsActive()
+    {
+        return m_isActive;
+    }
+
+    public float GetFrequency()
+    {
+        return m_frequency;
+    }
+
+    public int GetDelay()
+    {
+        return m_delay;
+    }
+
+    public void SetFrequency(float frequency)
+    {
+        m_frequency = frequency;
+    }
+
+
 }
