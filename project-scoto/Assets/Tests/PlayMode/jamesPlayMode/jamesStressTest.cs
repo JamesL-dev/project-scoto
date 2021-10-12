c0169965fe1794d15fa14965b553bbddfb9cf4cb
JamesL-dev <jmlasso2@nic.edu>
2021-10-11 22:26:39 -0700

    Update jamesStressTest.cs

--- project-scoto/Assets/Tests/PlayMode/jamesPlayMode/jamesStressTest.cs
+++ project-scoto/Assets/Tests/PlayMode/jamesPlayMode/jamesStressTest.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEditor;
 using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
-using UnityEditor;
-using UnityEngine.SceneManagement;
 public class JamesPlayModeTest
 {  
     [UnityTest]
