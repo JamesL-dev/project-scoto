c4f5fd18021775812dec603eb75b98d23804d756
JamesL-dev <jmlasso2@nic.edu>
2021-10-18 16:51:39 -0700

    Update code
    Refactor code to meet coding standards

--- project-scoto/Assets/Source/James/Pickups/Scoin.cs
+++ project-scoto/Assets/Source/James/Pickups/Scoin.cs
@@ -1,11 +1,29 @@
+/*
+* Filename: Scoin.cs
+* Developer: James Lasso
+* Purpose: Scoin pickup contains payload information and overrides
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Scoin powerup
+* Contains payload and overrides.
+*
+* Member variables: none
+*/
 public class Scoin : PowerUp
 {
     public int scoinBonus = 1;
 
+    /* Function that contains payload information.
+    *  Default powerup payload is overridden here
+    *
+    * Parameters: none
+    *
+    * Returns: none
+    */
     protected override void PowerUpPayload()
     {
         base.PowerUpPayload();
@@ -14,17 +32,28 @@ public class Scoin : PowerUp
         // playerStuff.setScoinAdjustment(addScoin);
     }
 
+    /* Function to destroy object when picked up
+    *
+    * Parameters: none
+    *
+    * Returns: none
+    */
     protected override void DestroySelfAfterDelay()
     {
         Destroy(gameObject); 
     }
 
 
-    // Update is called once per frame
+    /* Function that updates every frame.
+    *  In this case its used to animate the object.
+    *
+    * Parameters: none
+    *
+    * Returns: none
+    */
     void Update()
     {
-        // Spin scoin 
-        transform.Rotate(new Vector3(1f, 0f, 0f));
+        transform.Rotate(new Vector3(1f, 0f, 0f)); // Spin
         if (scoinBonus > 5)
         {
             scoinBonus = 5;
