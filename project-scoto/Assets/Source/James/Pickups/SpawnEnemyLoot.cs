34747e4ede45c31bdfce16b6eb677e3fc1822c5c
JamesL-dev <jmlasso2@nic.edu>
2021-10-20 15:59:45 -0700

    quick rename

--- project-scoto/Assets/Source/James/Pickups/SpawnEnemyLoot.cs
+++ project-scoto/Assets/Source/James/Pickups/SpawnEnemyLoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnLoot : MonoBehaviour
+public class SpawnEnemyLoot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
