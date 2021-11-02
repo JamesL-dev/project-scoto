6d991129d9811680191eb9ac6a593d77769228e3
ZachAttack6089 <70180035+ZachAttack6089@users.noreply.github.com>
2021-11-02 13:46:46 -0700

    Set doors to open when the other side is opened
    A few bugs need to be fixed, but it's in a working state

--- project-scoto/Assets/Source/Zach/LevelGeneration/Room.cs
+++ project-scoto/Assets/Source/Zach/LevelGeneration/Room.cs
@@ -19,6 +19,8 @@ using UnityEngine;
  * m_opener -- GameObject for a test pickup to open doors.
  * m_wallPositions -- Vector3 for the preset positions of the 4 walls of a room.
  * m_wallRotations -- Vector3 for the preset rotations of the 4 walls of a room.
+ * m_xPos -- Integer for the room's x position copied from the room's factory.
+ * m_zPos -- Integer for the room's z position copied from the room's factory.
  * m_doorList -- Array of booleans copied from the room's factory.
  * m_isCleared -- Boolean that stores if a room has been cleared or not.
  * m_timer -- Integer for the number of frames since the room was created.
@@ -30,6 +32,7 @@ public class Room : MonoBehaviour
     public float m_wallPosMultiplier;
 
     protected Vector3[] m_wallPositions = new Vector3[4], m_wallRotations = new Vector3[4];
+    protected int m_xPos, m_zPos;
     protected bool[] m_doorList = new bool[] {false, false, false, false};
     protected bool m_isCleared = false;
     protected int m_timer = 0;
@@ -53,50 +56,62 @@ public class Room : MonoBehaviour
             return;
         }
 
-        // Detect if all enemies are defeated.
-        int enemyCount = 0;
+        // Detect if all enemies are defeated, if the room had any.
         EnemySpawner enemySpawner = GetComponentInChildren<EnemySpawner>();
-        if (enemySpawner != null)
+        if (enemySpawner != null && enemySpawner.GetEnemyCount() == 0 && !m_isCleared)
         {
-            enemyCount = enemySpawner.GetEnemyCount();
-        }
-
-        // If so, open the doors.
-        if (enemyCount == 0 && !m_isCleared)
-        {
+            // If so, mark the room as cleared and open the doors.
             m_isCleared = true;
             for (int i = 0; i < 4; i++)
             {
                 if (m_doorList[i])
-                {
-                    // Delete old wall.
-                    Destroy(m_wallList[i].gameObject);
+                    OpenDoorInRoom(i);
+            }
 
-                    // Create new wall with a door.
-                    GameObject tempWall;
-                    tempWall = Instantiate(m_wallDoor, this.transform);
-                    tempWall.transform.position += m_wallPositions[i] * m_wallPosMultiplier;
-                    tempWall.transform.eulerAngles = m_wallRotations[i];
-                    tempWall.tag = m_wallDoor.tag;
-
-                    // Add wall to array.
-                    m_wallList[i] = tempWall;
-
-                    // Open door.
-                    m_wallList[i].GetComponentInChildren<Door>().OpenDoor();
-                }
+            // Also open the connected doors in adjacent rooms.
+            if (m_doorList[0])
+            {
+                // North door, so open south door in room with z + 1.
+                Room tempRoom = LevelGeneration.Inst().GetRoom(m_xPos, m_zPos + 1);
+                if (tempRoom != null)
+                    tempRoom.OpenDoorInRoom(2);
+            }
+            if (m_doorList[1])
+            {
+                // East door, so open west door in room with x + 1.
+                Room tempRoom = LevelGeneration.Inst().GetRoom(m_xPos + 1, m_zPos);
+                if (tempRoom != null)
+                    tempRoom.OpenDoorInRoom(3);
+            }
+            if (m_doorList[2])
+            {
+                // South door, so open north door in room with z - 1.
+                Room tempRoom = LevelGeneration.Inst().GetRoom(m_xPos, m_zPos - 1);
+                if (tempRoom != null)
+                    tempRoom.OpenDoorInRoom(0);
+            }
+            if (m_doorList[3])
+            {
+                // West door, so open east door in room with x - 1.
+                Room tempRoom = LevelGeneration.Inst().GetRoom(m_xPos - 1, m_zPos);
+                if (tempRoom != null)
+                    tempRoom.OpenDoorInRoom(1);
             }
         }
     }
 
-    /* Sets up the room by creating walls and doors and spawning the player.
+    /* Sets up the room by creating walls and doors.
      *
      * Parameters:
      * d -- Door list from ProtoRoom.
+     * x -- Integer for x position from ProtoRoom.
+     * z -- Integer for z position from ProtoRoom.
      */
-    public virtual void Init(bool[] d)
+    public virtual void Init(bool[] d, int x, int z)
     {
-        // Store the door list.
+        // Store the values from the ProtoRoom.
+        m_xPos = x;
+        m_zPos = z;
         m_doorList = d;
 
         // Generate walls.
@@ -112,6 +127,36 @@ public class Room : MonoBehaviour
             // Add wall to array.
             m_wallList[i] = tempWall;
         }
+    }
+
+    /* Opens a given door within the room.
+     *
+     * Parameters:
+     * i -- Integer for index of door from door list.
+     */
+    public void OpenDoorInRoom(int i)
+    {
+        // Stop if door was opened already.
+        if (m_wallList[i].tag == "WallDoor")
+        {
+            return;
+        }
+
+        // Delete old wall.
+        Destroy(m_wallList[i].gameObject);
+
+        // Create new wall with a door.
+        GameObject tempWall;
+        tempWall = Instantiate(m_wallDoor, this.transform);
+        tempWall.transform.position += m_wallPositions[i] * m_wallPosMultiplier;
+        tempWall.transform.eulerAngles = m_wallRotations[i];
+        tempWall.tag = m_wallDoor.tag;
+
+        // Add wall to array.
+        m_wallList[i] = tempWall;
+
+        // Open door.
+        m_wallList[i].GetComponentInChildren<Door>().OpenDoor();
     }
 
     /* Sets preset values for wall positions and rotations.
