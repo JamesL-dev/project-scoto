a1fe510399692da716087e7806a3a3639c854883
RodneyMcCoy <rbmj2001@outlook.com>
2021-12-07 13:19:20 -0800

    a
    a

--- project-scoto/Assets/Source/Rodney/Code/NavAgent.cs
+++ project-scoto/Assets/Source/Rodney/Code/NavAgent.cs
@@ -9,59 +9,41 @@ public class NavAgent : MonoBehaviour
 
     public static GameObject m_camera, m_player, m_thisGameObject;
     public static NavMeshAgent m_agent;
-    public static bool Active;
-    public static Vector3 destination;
-
-    private static bool deactivated;
+    public static Vector2 destination, position;
 
     void Start()
     {
-        m_thisGameObject = GetComponent<GameObject>();
+        m_thisGameObject = gameObject;
         m_player = GameObject.Find("Player");
         m_camera = GameObject.Find("Main Camera");
         m_agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
         gameObject.transform.position = m_player.transform.position;
         m_agent.autoTraverseOffMeshLink = false;
-        m_agent.speed = 2F;
-        deactivated = false;
+        m_agent.speed = 15F;
+        destination = new Vector3(0, 0, 20);
+        position = Vector2.zero;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        if(deactivated)
+    {    
+        if(position != Vector2.zero)
         {
-            Active = false;
-            deactivated = false;
-            gameObject.transform.position = Vector3.zero;
-            m_agent.SetDestination(Vector3.zero);
-        }
-        
-        if(Active)
-        {
-            m_agent.SetDestination(destination);
+            gameObject.transform.position = new Vector3(position.x, gameObject.transform.position.y, position.y);
+            position = Vector2.zero;
         }
 
-        CurrentlyActive = Active;
-
+        m_agent.SetDestination(new Vector3(destination.x, gameObject.transform.position.y, destination.y));
     }
 
-    public static void GoTo(Vector3 location)
-    {
-        destination = location;
-    }
+    public static void Teleport(Vector3 location) { position = new Vector2(location.x, location.z); }
 
-    public static void SetActive(bool value)
-    {
-        if(value && !Active)
-        {
-            Active = true;
-            m_thisGameObject.transform.position = m_player.transform.position;
-        }
-        if(!value && Active)
-        {
-            deactivated = true;
-        }
-    }
+    public static void Teleport(int x, int z) { position = new Vector2(x, z); }
+
+    public static void GoTo(Vector3 location) { destination = new Vector2(location.x, location.z); }
+
+    public static void GoTo(int x, int z) { destination = new Vector2(x, z); }
+
+    public static Vector3 getPosition() {return m_thisGameObject.transform.position;}
 }
